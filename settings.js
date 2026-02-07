5899d37c59af6ff566d6b27622a5afab73b976c8
Tomex777 <patrickcoker737@gmail.com>
2026-02-07 09:26:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Tomex Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "𝙏𝞗𝞛𝞢𝞦⁩☻️ Bot",
+  botOwner: '𝙏𝞗𝞛𝞢𝞦⁩☻️', // Your name
+  ownerNumber: '2348064790531', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
